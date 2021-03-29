9d3575c590c65cf097001a22a4f1cb93c2c57d29
Haal20 <haal20vo@student.ju.se>
2021-03-29 14:06:31 +0200

    testing eventlisteners

--- src/render-views.js
+++ src/render-views.js
@@ -4,6 +4,10 @@ let viewPlan = document.getElementById('businessPlan');
 let viewIdea = document.getElementById('projectIdea');
 let viewContact = document.getElementById('contact');
 
+
+main.innerHTML = 'Hej från förstasidan';
+
+
 viewPlan.addEventListener('click', (e) => {
     main.innerHTML = 'Hej från Affärsplan';
 });
