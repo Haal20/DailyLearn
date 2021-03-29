4d9051a9959d8d7af8964cf0b4a1878a63014952
Haal20 <haal20vo@student.ju.se>
2021-03-29 15:02:14 +0200

    Main har 3 olika vyer och ena är kontaktsida

--- src/render-views.js
+++ src/render-views.js
@@ -5,17 +5,23 @@ let viewIdea = document.getElementById('projectIdea');
 let viewContact = document.getElementById('contact');
 
 
-main.innerHTML = 'Hej från förstasidan';
+main.innerHTML = `<p>Detta är min förstasida. klicka vidare för att se mer ínformation om mitt projekt</p>`;
 
 
 viewPlan.addEventListener('click', (e) => {
-    main.innerHTML = 'Hej från Affärsplan';
+    main.innerHTML = `<h2>Affärsplan</h2>
+    <p>Detta är min affärsplan</p>`;
 });
 
 viewIdea.addEventListener('click', (e) => {
-    main.innerHTML = 'Hej från Produktidé';
+    main.innerHTML = `<h2>Produktidé</h2>
+    <p>Detta är min projektidé</p>`;
 });
 
 viewContact.addEventListener('click', (e) => {
-    main.innerHTML = 'Hej från Kontaktuppgifter';
+    main.innerHTML = `<h2>Mina kontaktuppgifter:</h2>
+    Alma Härlin <br>
+    Email: <a href="mailto: alma-harlin@hotmail.com">alma-harlin@hotmail.com</a><br>
+    Tel: <a href="tel:+739714337">0739-71 43 37</a>`;
 });
+
