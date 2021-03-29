964d6ff9b0aa816fd15793d7a6184eb223432138
Haal20 <haal20vo@student.ju.se>
2021-03-29 13:57:08 +0200

    added eventlistener and changed to correct name

--- src/render-views.js
+++ src/render-views.js
@@ -1,1 +1,17 @@
 //Page to handle rendering of views.
+let main = document.getElementById('main');
+let viewPlan = document.getElementById('businessPlan');
+let viewIdea = document.getElementById('projectIdea');
+let viewContact = document.getElementById('contact');
+
+viewPlan.addEventListener('click', (e) => {
+    main.innerHTML = 'Hej från Affärsplan';
+});
+
+viewIdea.addEventListener('click', (e) => {
+    main.innerHTML = 'Hej från Produktidé';
+});
+
+viewContact.addEventListener('click', (e) => {
+    main.innerHTML = 'Hej från Kontaktuppgifter';
+});
