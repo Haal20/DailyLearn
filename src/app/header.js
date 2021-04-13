aec9a00250b0152b8836ef9ea2e076459868d832
Haal20 <haal20vo@student.ju.se>
2021-04-13 13:47:46 +0200

    made a burger component that should toggle onClick

--- src/app/header.js
+++ src/app/header.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import BurgerMenu from './burger-menu';
 
 class Header extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            isToggel: false
+        };
+    }
+
+    handleClick() {
+        console.log('Clicked burger');
+        //isToggle=true otherwise isToggle=false
+      }
+
+
     render() {
         return (
             <div className="Header">
-            <div><button className="burgerMenu">|||</button></div>
+            <BurgerMenu onClick={()=>this.handleClick} />
             <h1>Tisdag</h1>
             <hr></hr>
             </div>
