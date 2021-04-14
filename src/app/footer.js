7d837149c5b568e68b7aaa18447dc19466badda4
Haal20 <haal20vo@student.ju.se>
2021-04-14 08:58:48 +0200

    footer buttons are logging out Clicked <thing being clicked> correct

--- src/app/footer.js
+++ src/app/footer.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import ToDoList from './to-do-list.js'
+import GameMap from './game-map.js'
+import StudyTips from './study-tips.js'
 
 class Footer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+        };
+        this.toDoList = this.toDoList.bind(this);
+        this.gameMap = this.gameMap.bind(this);
+        this.studyTips = this.studyTips.bind(this);
+    }
+
+    toDoList(){
+        console.log('Clicked Hem');
+    }
+
+    gameMap(){
+        console.log('Clicked Gemensamma');
+    }
+    
+    studyTips(){
+        console.log('Clicked fördjupning');
+    }
+
     render() {
         return (
             <div>
                 <hr></hr>
-                <button>
+                <button onClick={this.toDoList}>
                     hem
                 </button>
-                <button>
+                <button onClick={this.gameMap}>
                     gemensamma
                 </button>
-                <button>
+                <button onClick={this.studyTips}>
                     fördjupning
                 </button>
             </div>
---
+++ src/app/study-tips.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class StudyTips extends React.Component {
+    render() {
+        return (
+            <div>
+               Hello world
+            </div>
+        );
+    }
+}
+
+export default StudyTips
