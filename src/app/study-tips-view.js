43f39c12005c269e1f8af987554675f8d16bc984
Haal20 <haal20vo@student.ju.se>
2021-04-15 08:45:58 +0200

    Added form to studyTipsview

--- src/app/study-tips-view.js
+++ src/app/study-tips-view.js
@@ -1,12 +1,37 @@
 import React from 'react'
 
 class StudyTipsView extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      localString:''
+    };
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    const input = e.target.value;
+    this.setState({localString: input});
+  }
+
+  handleFormSubmit() {
+    console.log('handleFormSubmit');
+
+  }
+
     render() {
       return (
         <div>
-          <h2>Study Tips</h2>
+          <h2>Study Tips: Spara något i LocalStorage och skriv ut</h2>
           <br />
-          Här ska jag leka LocalStorage
+          <form onSubmit={this.handleFormSubmit}>
+            <label>
+            Spara LocalString: <br/> <input value={this.state.localString} onChange={this.handleChange} />
+            </label>
+            <button type="submit">Spara Sträng</button>
+          </form>
         </div>
       );
     }
