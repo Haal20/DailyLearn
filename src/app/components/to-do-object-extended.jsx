b79d4d5bcfba76a03ad5c694a9dfe5eb27674bd8
Haal20 <haal20vo@student.ju.se>
2021-05-21 09:19:39 +0200

    documenterade min återanvändningsbara komponent

--- src/app/components/to-do-object-extended.jsx
+++ src/app/components/to-do-object-extended.jsx
@@ -25,17 +25,24 @@ export class ToDoObjectExtended extends React.Component {
             <div className='createForm'>
                 <p>{this.context.todo.id}<br />Detta är ytterliggare information om {this.context.todo.assignmentName} ({this.context.todo.assignmentType})</p><br/>
                 
+                {/** Återanvändningsbar komponent. 
+                 * En knapp som tar två strängar, en handleClick och en boolean som props. */}
                 <ConfirmButton 
                 btnText="Radera" 
                 question="Är du säker?"  
                 handleClick={(val) => {
+                    /* if/elsen bestämmer vad confirmed ska vara tilldelat med. Beroende på vad användaren trycker på för knapp */
                     if(val === 'Ja'){
+                        /* Om man tryckt radera och tryck på Ja så körs onDeleteSubmit */
                     this.context.onDeleteSubmit(this.props.match.params.id)
                 }else if(val === 'confirmed'){
+                    /* Om man tryckt på radera så blir confirmed = true och knapparna Ja/Nej renderas ut */
                     this.setState({confirmed: true});
                 }else{
+                    /* Annars så renderas endast raderar knappen ut*/
                     this.setState({confirmed: false});
                 }}}
+                /* Här skickas confirmed med in i ConfirmButton */
                 confirmed = {this.state.confirmed} />
 
             </div>
